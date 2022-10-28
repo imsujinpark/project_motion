316ac21b543d130dbf1ea29d3e29dd9d66ba3edb
imsujinpark <imsujinpark@gmail.com>
2022-10-29 00:13:26 +0900

    feat: PageItemComponent created

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,27 @@
 import { ImageComponent } from "./components/page/item/image.js";
-import { PageComponent } from "./components/page/page.js";
+import { NoteComponent } from "./components/page/item/note.js";
+import { TodoComponent } from "./components/page/item/todo.js";
+import { VideoComponent } from "./components/page/item/video.js";
+import { PageComponent, Composable } from "./components/page/page.js";
+import { Component } from "./components/component.js";
 
 class App {
-    private readonly page: PageComponent;
+    private readonly page: Component & Composable;
     constructor(appRoot: HTMLElement) {
         this.page = new PageComponent();
         this.page.attachTo(appRoot);
 
         const image = new ImageComponent("Image Title", "https://picsum.photos/600/300");
-        image.attachTo(appRoot, "beforeend");
+        this.page.addChild(image);
+
+        const video = new VideoComponent("Video Title", "https://www.youtube.com/embed/fCO7f0SmrDc");
+        this.page.addChild(video);
+
+        const note = new NoteComponent("Note Title", "Note Body");
+        this.page.addChild(note);
+
+        const todo = new TodoComponent("Todo Title", "Todo Item");
+        this.page.addChild(todo);
     }
 }
 // 이런 경우에만 사용해야 하는 type assertion
