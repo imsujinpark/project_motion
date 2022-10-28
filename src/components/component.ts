a242d2be974f2bd131bd028023f08ae3e7473ab8
imsujinpark <imsujinpark@gmail.com>
2022-10-28 19:25:53 +0900

    refactor: component class created from page.ts + image.ts's redundant code

---
+++ src/components/component.ts
@@ -0,0 +1,20 @@
+export interface Component {
+    attachTo(parent: HTMLElement, position?: InsertPosition): void;
+}
+
+/**
+ * Encapsulate the HTML element creation
+ * BaseComponent 사용할 때 조금 더 다양한 HTML Subcomponent를 사용할 수 있도록 Generic 사용
+ */
+export class BaseComponent<T extends HTMLElement> implements Component {
+    protected readonly element: T; // 자식컴포넌트에서만 접근 가능하고, 한번 만들어지면 읽기만 가능하도록
+    constructor(htmlString: string) {
+        const template = document.createElement("template");
+        template.innerHTML = htmlString;
+        this.element = template.content.firstElementChild! as T;
+    }
+
+    attachTo(parent: HTMLElement, position: InsertPosition = "afterbegin") {
+        parent.insertAdjacentElement(position, this.element)
+    }
+}
--- src/components/page/item/image.ts
+++ src/components/page/item/image.ts
@@ -1,14 +1,11 @@
-export class ImageComponent {
-    private element: HTMLElement;
+import { BaseComponent } from "../../component";
+
+export class ImageComponent extends BaseComponent<HTMLElement> {
     constructor(title: string, url: string) {
-        const template = document.createElement("template");
-        template.innerHTML = `<section class="image">
-        <div class="image__holder">
-            <img class="image__thumbnail">
-        </div>
-        <p class="image__title"></p>
-    </section>`
-        this.element = template.content.firstElementChild! as HTMLElement; // section 안 내용 element가 있을 수도 있고 null 일수도 있지만 여기선 null 아닌걸 우리가 암(!)
+        super(`<section class="image">
+                    <div class="image__holder"><img class="image__thumbnail"></div>
+                    <p class="image__title"></p>
+                </section>`);
 
         const imageElement = this.element.querySelector(".image__thumbnail")! as HTMLImageElement; // null이 아닌 걸 우리가 아니 image element로 캐스팅
         imageElement.src = url;
@@ -16,9 +13,5 @@ export class ImageComponent {
 
         const titleElement = this.element.querySelector(".image__title")! as HTMLParagraphElement;
         titleElement.textContent = title;
-    }
-    // 기본값은 afterbegin
-    attachTo(parent: HTMLElement, position: InsertPosition = "afterbegin") {
-        parent.insertAdjacentElement(position, this.element)
     }
 }
