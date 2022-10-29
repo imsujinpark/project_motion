2260fa30013065c8dbd14fa7b0b661035eae18b2
imsujinpark <imsujinpark@gmail.com>
2022-10-29 13:58:11 +0900

    feat: dialog input component added

---
+++ src/components/dialog/dialog.ts
@@ -0,0 +1,49 @@
+import { BaseComponent, Component } from "../component.js";
+import { Composable } from "../page/page.js";
+
+type OnCloseListener = () => void;
+type OnSubmitListener = () => void;
+
+// closeListener와 submitListener를 외부로 전달 받아서 등록된 listener가 있다면 실행해줄 것
+export class InputDialog extends BaseComponent<HTMLElement> implements Composable {
+    closeListener?: OnCloseListener;
+    submitListener?: OnSubmitListener;
+
+    constructor() {
+        super(`<dialog class="dialog">
+                    <div class="dialog__container">
+                        <button class="close">&times;</button>
+                        <div id="dialog__body"></div>
+                        <button class="dialog__submit">ADD</button>
+                    </div>
+                </dialog>`)
+
+        // const dialogContainer = this.element.querySelector(".dialog__container")! as HTMLElement;
+        // dialogContainer.onclick = (event: any) => {
+        //     // const target = event.target as Element;
+        //     if (!event.target.matches(".dialog__container")) return;
+        //     this.closeListener && this.closeListener();
+        // }
+
+        const closeBtn = this.element.querySelector(".close")! as HTMLElement;
+        // closeBtn.addEventListener("click", ""); // 버튼을 다른곳에서도 사용한다면 이렇게 구현하는게 낫다.
+        closeBtn.onclick = () => {
+            this.closeListener && this.closeListener();
+        }
+
+        const submitBtn = this.element.querySelector(".dialog__submit")! as HTMLElement;
+        submitBtn.onclick = () => {
+            this.submitListener && this.submitListener();
+        }
+    }
+    setOnCloseListener(listener: OnCloseListener) {
+        this.closeListener = listener;
+    }
+    setOnSubmitListener(listener: OnSubmitListener) {
+        this.submitListener = listener;
+    }
+    addChild(child: Component) {
+        const body = this.element.querySelector("#dialog__body")! as HTMLElement;
+        child.attachTo(body);
+    }
+}
