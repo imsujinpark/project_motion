674c37a7a6e7157215df6c15b2a964b9832361a6
imsujinpark <imsujinpark@gmail.com>
2022-10-29 16:36:40 +0900

    chore: dist commited

---
+++ dist/app.js
@@ -0,0 +1,38 @@
+import { ImageComponent } from "./components/page/item/image.js";
+import { NoteComponent } from "./components/page/item/note.js";
+import { TodoComponent } from "./components/page/item/todo.js";
+import { VideoComponent } from "./components/page/item/video.js";
+import { PageComponent, PageItemComponent } from "./components/page/page.js";
+import { InputDialog } from "./components/dialog/dialog.js";
+import { MediaSectionInput } from "./components/dialog/input/media-input.js";
+import { TextSectionInput } from "./components/dialog/input/text-input.js";
+
+class App {
+    constructor(appRoot, dialogRoot) {
+        this.dialogRoot = dialogRoot;
+        this.page = new PageComponent(PageItemComponent);
+        this.page.attachTo(appRoot);
+        this.bindElementToDialog("#new-image", MediaSectionInput, (input) => new ImageComponent(input.title, input.url));
+        this.bindElementToDialog("#new-video", MediaSectionInput, (input) => new VideoComponent(input.title, input.url));
+        this.bindElementToDialog("#new-note", TextSectionInput, (input) => new NoteComponent(input.title, input.body));
+        this.bindElementToDialog("#new-todo", TextSectionInput, (input) => new TodoComponent(input.title, input.body));
+    }
+    bindElementToDialog(selector, InputComponent, makeSection) {
+        const element = document.querySelector(selector);
+        element.addEventListener("click", () => {
+            const dialog = new InputDialog();
+            const inputSection = new InputComponent();
+            dialog.addChild(inputSection);
+            dialog.attachTo(this.dialogRoot);
+            dialog.setOnCloseListener(() => {
+                dialog.removeFrom(this.dialogRoot);
+            });
+            dialog.setOnSubmitListener(() => {
+                const element = makeSection(inputSection);
+                this.page.addChild(element);
+                dialog.removeFrom(this.dialogRoot);
+            });
+        });
+    }
+}
+new App(document.querySelector(".document"), document.body);
